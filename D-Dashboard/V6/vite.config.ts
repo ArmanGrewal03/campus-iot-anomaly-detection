f96cba0a8d997d94b5ee3112e2d0747bce0d0779
Arman Grewal <armangrewal01@gmail.com>
2026-02-07 13:54:52 -0500

    Enforce strict ports for testing

--- D-Dashboard/V6/vite.config.ts
+++ D-Dashboard/V6/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5174,
+    port: 5173,
+    strictPort: true, // Fail if port is already in use instead of trying another port
   },
 });
